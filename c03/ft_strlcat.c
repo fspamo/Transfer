87ed78333b95e3ac03fe61ffc171367e68575c68
fspamo <113922120+fspamo@users.noreply.github.com>
2025-10-08 19:19:22 +0300

    ft_strlcat is fixed and understood properly

--- c03/ft_strlcat.c
+++ c03/ft_strlcat.c
@@ -9,36 +9,34 @@ int ft_strlen(char *str)
 		i++;
 	return (i);
 }
+
 unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	int	dest_length = 0;
-	int	src_length = 0;
-	int i = 0;
-	
-	while(dest_length < size && dest[dest_length])
+	unsigned int	src_size;
+	unsigned int	dest_size;
+	unsigned int	i;
+
+	dest_size = ft_strlen(dest);
+	src_size = ft_strlen(src);
+	i = 0;
+
+	if (dest_size >= size)
+		return size + src_size;
+
+	while (src[i] && (dest_size + i + 1) < size)
 	{
-		dest_length;
-	}
-	if(dest_length == size)
-	{
-		return (size + src_length);
-	}
-	while((dest_length + i + 1) < size && src[i])
-	{
-		dest[dest_length + i] = src[i];
+		dest[dest_size + i] = src[i];
 		i++;
 	}
-	if (dest_length + i < size)
-		dest[dest_length + i] = '\0';
 
-	return (dest_length + src_length);
+	dest[dest_size + i] = '\0';
+	return dest_size + src_size;
 }
 
 int main(void)
 {
-	char src[] = "something";
-	char dest[40] = "in the way";
-	int size = 17;
-
-	ft_strlcat(dest, src, size);
+	char src[] = "world";
+	char dest[20] = "helloksdfkj";
+	printf("%d", ft_strlcat(dest, src, 9));
+	//ft_strlcat(dest, src, 40);
 }
