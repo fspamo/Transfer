1cb52ef766d7ca952b7589825dc4570ea07800d7
fspamo <113922120+fspamo@users.noreply.github.com>
2025-10-13 07:26:56 +0300

    yea

---
+++ c06/ft_print_params.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+	int i;
+	int j;
+
+	i = 1;
+	j = 0;
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			write(1, &av[i][j], 1);
+			j++;
+		}
+		i++;
+		write(1, "\n", 1);
+	}
+}
---
+++ c06/ft_rev_params.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int ac, char **av)
+{
+	int i;
+	int j;
+
+	i = ac - 1;
+	j = 0;
+	while (i > 0)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			write(1, &av[i][j], 1);
+			j++;
+		}
+		i--;
+		write(1, "\n", 1);
+	}
+}
